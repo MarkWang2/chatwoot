b7523f867e4e88fa3300abcc0e65107cba4e9925
Mark.wang2@activenetwork.com <Mark.wang2@activenetwork.com>
2025-12-05 07:25:52 +0800

    refactor: Add getPaid method to handle not paid message

--- app/javascript/dashboard/components-next/message/bubbles/Text/FormattedContent.vue
+++ app/javascript/dashboard/components-next/message/bubbles/Text/FormattedContent.vue
@@ -4,7 +4,7 @@ import { useMessageContext } from '../../provider.js';
 
 import MessageFormatter from 'shared/helpers/MessageFormatter.js';
 import { MESSAGE_VARIANTS } from '../../constants';
-import { useStoreGetters } from 'dashboard/composables/store';
+import { useMapGetter } from 'dashboard/composables/store.js';
 
 const props = defineProps({
   content: {
@@ -15,22 +15,17 @@ const props = defineProps({
 
 const { variant } = useMessageContext();
 
-const getters = useStoreGetters();
-// const globalConfig = computed(() => getters['globalConfig/get'].value);
-const currentUser = computed(() => getters.getCurrentUser.value);
+const paid = useMapGetter('getPaid');
 
 const formattedContent = computed(() => {
   if (variant.value === MESSAGE_VARIANTS.ACTIVITY) {
     return props.content;
   }
-  debugger;
-
-  currentUser;
   return new MessageFormatter(props.content).formattedMessage;
 });
 </script>
 
 <template>
   <span v-dompurify-html="formattedContent" class="prose prose-bubble" />
-  <div>mark test "😀" dddddd</div>
+  <div v-if="!paid">Not paid</div>
 </template>
